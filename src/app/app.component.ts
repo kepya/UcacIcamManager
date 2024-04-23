6f11988318dfbb07502b613a476b8dc81012c76a
DarylNoupik <daryl.kuitche@2023.ucac-icam.com>
2024-04-23 21:11:04 +0100

    Modification des textes et du referencement SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,35 @@ import { LoaderService } from './shared/services/loader.service';
 import { LoginComponent } from './login/login.component';
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, NavigationEnd, NavigationStart, Router } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  NavigationStart,
+  Router,
+} from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
-import { filter, map } from "rxjs";
+import { filter, map } from 'rxjs';
 import { AuthService } from './shared/services/auth.service';
 import { BaseUrlService } from './shared/services/base-url.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: []
+  styles: [],
 })
 export class AppComponent implements OnInit {
-
   isExtend: boolean = true;
   isLoading: boolean = true;
   isLoginPage: boolean = true;
   url: string = '';
   title = 'admin-ucac-dashboard';
 
-  constructor(private router: Router, private titleService: Title, private primengConfig: PrimeNGConfig, private loaderService: LoaderService) {
-  }
+  constructor(
+    private router: Router,
+    private titleService: Title,
+    private primengConfig: PrimeNGConfig,
+    private loaderService: LoaderService
+  ) {}
 
   checkSidebarMode(isExtend: boolean) {
     this.isExtend = isExtend;
@@ -30,33 +38,35 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.primengConfig.ripple = true;
-    this.router.events.pipe(
-      filter((event) => event instanceof NavigationEnd),
-      map(() => {
-        let route: ActivatedRoute = this.router.routerState.root;
-        let routeTitle = '';
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let route: ActivatedRoute = this.router.routerState.root;
+          let routeTitle = '';
 
-        while (route!.firstChild) {
-          route = route.firstChild;
+          while (route!.firstChild) {
+            route = route.firstChild;
+          }
+
+          if (route.snapshot.data['title']) {
+            routeTitle = route!.snapshot.data['title'];
+          }
+
+          return routeTitle;
+        })
+      )
+      .subscribe((title: string) => {
+        if (title) {
+          this.titleService.setTitle(`GIC - ${title}`);
+          this.url = title;
         }
-
-        if (route.snapshot.data['title']) {
-          routeTitle = route!.snapshot.data['title'];
-        }
-
-        return routeTitle;
-      })
-    ).subscribe((title: string) => {
-      if (title) {
-        this.titleService.setTitle(`My App - ${title}`);
-        this.url = title;
-      }
-    });
+      });
     this.loaderService.isLoading.subscribe({
       next: (loading: boolean) => {
         this.isLoading = loading;
-      }
-    })
+      },
+    });
   }
 
   onLoadPage(component: any) {
